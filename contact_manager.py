dde571fde5e3df8044314ca525bb727291100d5b
victoriayiyau <victoriayiyau@yahoo.com>
2016-05-31 18:56:17 -0700

    Add files via upload

--- contact_manager.py
+++ contact_manager.py
@@ -7,8 +7,6 @@ def add_contact(first_name, last_name):
 	new_contact = Contact(first_name,last_name) #instantiate instance of contact
 	contact_list.append(new_contact) #add contact to contact_list
 
-# def remove
-# #check if contact exists in list, if yes, remove; if no, display warning
 def remove_contact(first_name, last_name):
 	global contact_list
 	i = 0
@@ -19,43 +17,169 @@ def remove_contact(first_name, last_name):
 			contact_deleted += 1
 			print "%s %s has been removed." % (first_name,last_name)
 		else:
-			i += 1
+			i += 1 #continues to iterate
 	if contact_deleted == 0:
 		print "%s %s was not on the contact list." % (first_name,last_name)
 	else:
 		print "Here is your" #display contact list - insert function display_sorted
-			
-def edit(first_name, last_name):
-	global contact_list
-	old_contact = Contact(first_name, last_name)
-	if old_contact in contact_list:
-		new_contact = contact_list.index(old_contact)
-		print "%s has replaced %s in the contact list." % (new_contact, old_contact)
-	else:
-		print "%s is not in the contact list." % (old_contact)
-		
-# def edit
-# #check if contact exists in list, if yes, edit contact attributes, if no, create new instance of contact
 
-# def display_sorted
-# #print contact_list
+def search_contact(first_name, last_name):
+	#searches contact list 
+	global contact_list
+	while True:
+		print """Search Contact by: 
+				1 - First Name
+				2 - Last Name
+				3 - Mobile Phone
+				4 - Work Phone
+				5 - Email
+				6 - Twitter
+				7 - Return to Main Menu"""
+		search_option = raw_input("Please make your selection: ")
+		search_text = raw_input("Enter text to search: ")
+		i = 0
+		while i < len(contact_list):
+			if search_option == "1" and search_text == contact_list[i].first_name:
+				show_contact(contact_list[i])
+			elif search_option == "2" and search_text == contact_list[i].last_name:
+				show_contact(contact_list[i])
+			elif search_option == "3" and search_text == contact_list[i].mobile_number:
+				show_contact(contact_list[i])
+			elif search_option == "4" and search_text == contact_list[i].work_number:
+				show_contact(contact_list[i])
+			elif search_option == "5" and search_text == contact_list[i].email:
+				show_contact(contact_list[i])
+			elif search_option == "6" and search_text == contact_list[i].twitter_handle:
+				show_contact(contact_list[i])
+			elif search_option == "7":
+				break
+			
+def show_contact(contact_object):
+	print """First Name: %s 
+			 Last Name: %s 
+			 Mobile Phone: %s
+			 Work Phone: %s
+			 Email: %s
+			 Twitter: %s 
+			 """ % (contact_object.first_name, contact_object.last_name, contact_object.mobile_number, contact_object.work_number, contact_object.email, contact_object.twitter_handle)
+
+def print_contact_list():
+	#prints entire contact list
+	global contact_list
+	i = 0 
+	while i < len(contact_list):
+		show_contact(contact_list[i])
+		i +=1
+
+def edit_contact(first_name, last_name):
+	global contact_list
+	i = 0
+	while i < len(contact_list):
+		if contact_list[i].last_name == last_name and contact_list[i].first_name == first_name:
+			show_contact(contact_list[i])
+			while True:
+				print """Edit Menu: 
+						1 - First Name
+						2 - Last Name
+						3 - Mobile Phone
+						4 - Work Phone
+						5 - Email
+						6 - Twitter
+						7 - Return to Main Menu"""
+				edit_menu_selection = raw_input("Please enter which information you would like to edit: ")
+				if edit_menu_selection == "1": 
+					#change First Name
+					new_info = raw_input("Type new First Name: ")
+					if new_info.strip(" ") == "":
+						print "Invalid input. Please try again."
+					else:
+						contact_list[i].edit_first_name(new_info)
+						print "First Name changed to %s." % (new_info)
+				elif edit_menu_selection == "2":
+					#change Last Name
+					new_info = raw_input("Type new Last Name: ")
+					if new_info.strip(" ") == "":
+						print "Invalid input. Please try again."
+					else:
+						contact_list[i].edit_last_name(new_info)
+						print "Last Name changed to %s." % (new_info)
+				elif edit_menu_selection == "3":
+					#change mobile phone
+					new_info = raw_input("Type new Mobile Phone Number: ")
+					if new_info.strip(" ") == "":
+						print "Invalid input. Please try again."
+					else:
+						contact_list[i].edit_mobile_number(new_info)
+						print "Mobile Number changed to %s." % (new_info)
+				elif edit_menu_selection == "4":
+					#change work phone
+					new_info = raw_input("Type new Work Phone Number: ")
+					if new_info.strip(" ") == "":
+						print "Invalid input. Please try again."
+					else:
+						contact_list[i].edit_work_number(new_info)
+						print "Work Number changed to %s." % (new_info)
+				elif edit_menu_selection == "5":
+					#change email
+					new_info = raw_input("Type new Email Address: ")
+					if new_info.strip(" ") == "":
+						print "Invalid input. Please try again."
+					else:
+						contact_list[i].edit_email(new_info)
+						print "Email changed to %s." % (new_info)
+				elif edit_menu_selection == "6":
+					#change twitter handle
+					new_info = raw_input("Type new Twitter Handle: ")
+					if new_info.strip(" ") == "":
+						print "Invalid input. Please try again."
+					else:
+						contact_list[i].edit_twitter(new_info)
+						print "Twitter Handle changed to %s." % (new_info)
+				elif edit_menu_selection == "7":
+					break 
+				else:
+					print "Invalid option. Please make another selection."
+		i +=1
 
 def main():
-	first_name = "Wendy" #raw_input("Please type contact's first name: ")
-	last_name = "Brown" #raw_input ("Please type contact's last name: ")
-	print add_contact(first_name, last_name)
-	print remove_contact(first_name, last_name)
-	# # edit(first_name, last_name)
-	# return contact_list
-
-main()
-
-
-
-
+	while True:
+		print """	
+		Contact Manager Main Menu: 
+		1 - Add New Contact
+		2 - Remove Existing Contact
+		3 - Edit Existing Contact
+		4 - Show all Existing Contacts 				
+		5 - Search Contact List
+		6 - Exit
+		"""
+		menu_selection = raw_input("Please enter your selection from the Main Menu: ")
+		if menu_selection == "1":
+			#add contact
+			first_name = raw_input("Please enter contact's First Name: ")
+			last_name = raw_input("Please enter contact's Last Name: ")
+			add_contact(first_name,last_name)
+		elif menu_selection == "2":
+			#remove contact
+			first_name = raw_input("Please enter contact's First Name: ")
+			last_name = raw_input("Please enter contact's Last Name: ")
+			remove_contact(first_name,last_name)
+		elif menu_selection == "3":
+			#edit contact
+			first_name = raw_input("Please enter contact's First Name: ")
+			last_name = raw_input("Please enter contact's Last Name: ")
+			edit_contact(first_name,last_name)
+		elif menu_selection == "4":
+			#show all contacts
+			print_contact(contact_list)
+		elif menu_selection == "5":
+			#search contact list
+			search_contact(first_name,last_name)
+		elif menu_selection =="6":
+			break
 	
-	# more_info = raw_input("Do you want to add more contact details?")
-	# if more_info.upper == "YES":
-	# 	edit()
-	# else:
-	# 	print new_contact
+
+if __name__ == '__main__':
+	main()
+
+
+
